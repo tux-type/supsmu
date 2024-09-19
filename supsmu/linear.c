7a553e03e38f957925c00a210159b618aa9a7c1b
Tux <177398239+tux-type@users.noreply.github.com>
2024-09-19 20:01:05 +0100

    Refactor linear to use structs

--- supsmu/linear.c
+++ supsmu/linear.c
@@ -1,12 +1,13 @@
+#include "linear.h"
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void linear(const float_t *x, const float_t w, const float_t b, float_t *yhat,
+void linear(const float_t *x, const Params *params, float_t *yhat,
             const size_t len) {
   for (uint32_t i = 0; i < len; i++) {
-    yhat[i] = w * x[i] + b;
+    yhat[i] = params->w * x[i] + params->b;
   }
 }
 
@@ -19,57 +20,57 @@ float_t l2(const float_t *y, const float_t *yhat, const size_t len) {
   return loss;
 }
 
-void grads(const float_t *x, const float_t *y, const float_t w, const float_t b,
-           float_t *dj_dw, float_t *dj_db, const size_t len) {
-  *dj_dw = 0;
-  *dj_db = 0;
+void calc_grads(const float_t *x, const float_t *y, const Params *params,
+                Grads *grads, const size_t len) {
+  grads->dj_dw = 0;
+  grads->dj_db = 0;
 
   float_t *yhat = malloc(sizeof(float_t) * len);
   if (yhat == NULL) {
     printf("Failed to allocate %zu length array for predictions\n", len);
     exit(EXIT_FAILURE);
   }
-  linear(x, w, b, yhat, len);
+  linear(x, params, yhat, len);
 
   for (size_t i = 0; i < len; i++) {
-    *dj_dw += (yhat[i] - y[i]) * x[i];
-    *dj_db += (yhat[i] - y[i]);
+    grads->dj_dw += (yhat[i] - y[i]) * x[i];
+    grads->dj_db += (yhat[i] - y[i]);
   }
-  *dj_dw = *dj_dw / len;
-  *dj_db = *dj_db / len;
+  grads->dj_dw = grads->dj_dw / len;
+  grads->dj_db = grads->dj_db / len;
 
   free(yhat);
 };
 
-void update(float_t *w, float_t *b, const float_t dj_dw, const float_t dj_db,
-            const float_t alpha) {
-  *w = *w - alpha * dj_dw;
-  *b = *b - alpha * dj_db;
+void update(Params *params, Grads *grads, const float_t alpha) {
+  params->w = params->w - alpha * grads->dj_dw;
+  params->b = params->b - alpha * grads->dj_db;
 }
 
-void gradient_descent(const float *x, const float_t *y, float_t *w, float_t *b,
-                      const float_t alpha, const size_t num_iters,
-                      float_t (*cost_fn)(const float_t *y, const float_t *yhat,
-                                         const size_t len),
-                      void (*grad_fn)(const float_t *x, const float_t *y,
-                                      const float_t w, const float_t b,
-                                      float_t *dj_dw, float_t *dj_db,
-                                      const size_t len),
-                      const size_t len) {
+Params gradient_descent(
+    const float *x, const float_t *y, const float_t alpha,
+    const size_t num_iters,
+    float_t (*cost_fn)(const float_t *y, const float_t *yhat, const size_t len),
+    void (*grad_fn)(const float_t *x, const float_t *y, const Params *params,
+                    Grads *grads, const size_t len),
+    const size_t len) {
   float_t dj_dw = 0;
   float_t dj_db = 0;
-  for (size_t i = 0; i < num_iters; i++) {
-    grad_fn(x, y, *w, *b, &dj_dw, &dj_db, len);
-    grad_fn(x, y, *w, *b, &dj_dw, &dj_db, len);
+  Params params = {.w = 0, .b = 0};
+  Grads grads = {.dj_dw = 0, .dj_db = 0};
 
-    update(w, b, dj_dw, dj_db, alpha);
+  for (size_t i = 0; i < num_iters; i++) {
+    grad_fn(x, y, &params, &grads, len);
+
+    update(&params, &grads, alpha);
   }
+  return params;
 }
 
-void fit(float_t *x, float_t *y, float_t *w, float_t *b, size_t len) {
-  *w = 0;
-  *b = 0;
+Params fit(float_t *x, float_t *y, size_t len) {
   float_t alpha = 0.01;
   size_t num_iters = 1000000;
-  gradient_descent(x, y, w, b, alpha, num_iters, &l2, &grads, len);
+  Params params =
+      gradient_descent(x, y, alpha, num_iters, &l2, &calc_grads, len);
+  return params;
 }
--- supsmu/linear.h
+++ supsmu/linear.h
@@ -2,4 +2,14 @@
 #include <stddef.h>
 #pragma once
 
-void fit(float_t *, float_t *, float_t *, float_t *, size_t);
+typedef struct {
+  float_t w;
+  float_t b;
+} Params;
+
+typedef struct {
+  float_t dj_dw;
+  float_t dj_db;
+} Grads;
+
+Params fit(float_t *, float_t *, size_t);
