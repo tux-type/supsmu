e4eb412fb0e64910e9146592b91b7dae43fe4987
Tux <177398239+tux-type@users.noreply.github.com>
2024-10-19 15:26:37 +0100

    Update tests to allocate params within test

--- supsmu/test.c
+++ supsmu/test.c
@@ -2,47 +2,69 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 6
+
+void check_alloc(void *p, size_t len) {
+  if (p == NULL) {
+    printf("Failed to allocate %zu length array\n", len);
+    exit(EXIT_FAILURE);
+  }
+}
 
 void test_fit_local_linear(void) {
 
   // POSITIVE SLOPE
-  float_t x1[6] = {0, 1, 2, 3, 4, 5};
-  float_t y1[6] = {5, 4, 3, 2, 1, 0};
+  float_t x1[] = {0, 1, 2, 3, 4, 5};
+  float_t y1[] = {5, 4, 3, 2, 1, 0};
   int j1 = 2;
-  Params *params1 = fit_local_linear(x1, y1, 6, j1);
-  printf("w1: %f\n", params1->w);
-  printf("b1: %f\n", params1->b);
+  Params *local_params1 = malloc(sizeof(Params) * N);
+  check_alloc(local_params1, N);
+
+  fit_local_linear(local_params1, x1, y1, 6, j1);
+  printf("w1: %f\n", local_params1->w);
+  printf("b1: %f\n", local_params1->b);
 
   for (size_t i = 0; i < 6; i++) {
-    assert(-0.01 < (params1->w - (-1)) && (params1->w - (-1)) < 0.01);
-    assert(-0.01 < (params1->b - 5) && (params1->b - 5) < 0.01);
+    assert(-0.01 < (local_params1->w - (-1)) && (local_params1->w - (-1)) < 0.01);
+    assert(-0.01 < (local_params1->b - 5) && (local_params1->b - 5) < 0.01);
   }
+  free(local_params1);
 
   // NEGATIVE SLOPE
   float_t x2[6] = {0, 1, 2, 3, 4, 5};
   float_t y2[6] = {0, 1, 2, 3, 4, 5};
   int j2 = 2;
-  Params *params2 = fit_local_linear(x2, y2, 6, j2);
-  printf("w2: %f\n", params2->w);
-  printf("b2: %f\n", params2->b);
+  Params *local_params2 = malloc(sizeof(Params) * N);
+  check_alloc(local_params2, N);
+
+  fit_local_linear(local_params2, x2, y2, 6, j2);
+  printf("w2: %f\n", local_params2->w);
+  printf("b2: %f\n", local_params2->b);
 
   for (size_t i = 0; i < 6; i++) {
-    assert(-0.01 < (params2->w - 1) && (params2->w - 1) < 0.01);
-    assert(-0.01 < (params2->b - 0) && (params2->b - 0) < 0.01);
+    assert(-0.01 < (local_params2->w - 1) && (local_params2->w - 1) < 0.01);
+    assert(-0.01 < (local_params2->b - 0) && (local_params2->b - 0) < 0.01);
   }
+  free(local_params2);
 
   // ZERO SLOPE
   float_t x3[6] = {0, 1, 2, 3, 4, 5};
   float_t y3[6] = {1, 1, 1, 1, 1, 1};
   int j3 = 2;
-  Params *params3 = fit_local_linear(x3, y3, 6, j3);
-  printf("w2: %f\n", params3->w);
-  printf("b2: %f\n", params3->b);
+  Params *local_params3 = malloc(sizeof(Params) * N);
+  check_alloc(local_params3, N);
+
+  fit_local_linear(local_params3, x3, y3, 6, j3);
+  printf("w2: %f\n", local_params3->w);
+  printf("b2: %f\n", local_params3->b);
 
   for (size_t i = 0; i < 6; i++) {
-    assert(-0.01 < (params3->w - 0) && (params3->w - 0) < 0.01);
-    assert(-0.01 < (params3->b - 1) && (params3->b - 1) < 0.01);
+    assert(-0.01 < (local_params3->w - 0) && (local_params3->w - 0) < 0.01);
+    assert(-0.01 < (local_params3->b - 1) && (local_params3->b - 1) < 0.01);
   }
+  free(local_params3);
 }
 
 int main(void) {
