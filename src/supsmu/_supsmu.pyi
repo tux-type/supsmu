4b96694bd251d1bfc368ec37bcc07e859c98fef4
Tux <177398239+tux-type@users.noreply.github.com>
2024-12-22 14:58:13 +0000

    Update type annotations with new function arguments

--- src/supsmu/_supsmu.pyi
+++ src/supsmu/_supsmu.pyi
@@ -1,4 +1,11 @@
 import numpy.typing as npt
 import numpy as np
 
-def supsmu(x: npt.NDArray[np.float32], y: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]: ...
+def supsmu(
+    x: npt.NDArray[np.float32],
+    y: npt.NDArray[np.float32],
+    wt: npt.NDArray[np.float32] | None = None,
+    span: float = 0,
+    periodic: bool = False,
+    bass: float = 0,
+) -> npt.NDArray[np.float32]: ...
