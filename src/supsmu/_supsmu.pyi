4cd359e38c25e3e9be93469c9b2b567f5466c753
Tux <177398239+tux-type@users.noreply.github.com>
2025-01-17 19:36:13 +0000

    Update type hints in stub file

--- src/supsmu/_supsmu.pyi
+++ src/supsmu/_supsmu.pyi
@@ -2,9 +2,9 @@ import numpy.typing as npt
 import numpy as np
 
 def supsmu(
-    x: npt.NDArray[np.floating],
-    y: npt.NDArray[np.floating],
-    wt: npt.NDArray[np.floating] | None = None,
+    x: npt.NDArray[np.floating | np.integer],
+    y: npt.NDArray[np.floating | np.integer],
+    wt: npt.NDArray[np.floating | np.integer] | None = None,
     span: float = 0,
     periodic: bool = False,
     bass: float = 0,
